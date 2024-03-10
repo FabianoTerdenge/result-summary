ca5fd764861a32eb3e84cf90e8cf93b15efc3c47
Fabiano Terdenge <ft748572@fh-muenster.de>
2024-03-10 23:26:00 +0100

    load data from json and start styling

---
+++ script.js
@@ -0,0 +1,36 @@
+//add data.json to html
+let summary = document.getElementById("summary");
+console.log("Summary:", summary);
+fetch('data.json')
+    .then((response) => response.json())
+    .then((json) => {
+        json.forEach(summaryItem => {
+            //create element 
+            let left = document.createElement("div");
+            left.style.display ="flex";
+            let right = document.createElement("div");
+            let element = document.createElement("div");
+            element.className = "summaryItem";
+            element.style.backgroundColor = summaryItem.backgroundColor;
+            let name = document.createElement("span");
+            name.innerHTML = summaryItem.category;
+            name.style.color = summaryItem.color;
+            let score = document.createElement("span");
+            score.innerHTML = summaryItem.score;
+            let scoreOf = document.createElement("span");
+            scoreOf.innerHTML = " / 100";
+            let img = document.createElement("img");
+            img.src = summaryItem.icon;
+            img.alt = summaryItem.icon.split("/").splice(-1)[0];
+
+            left.appendChild(img);
+            left.appendChild(name);
+            right.appendChild(score);
+            right.appendChild(scoreOf);
+            element.appendChild(left);
+            element.appendChild(right);
+            summary.appendChild(element);
+        });
+    }
+    );
+
